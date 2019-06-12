3c3a495490fdd6da22f312385ad69021c00581c5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 22:12:39 +0800

    Add files via upload

--- Script-019c-3D-Scatterplot-Maria.py
+++ Script-019c-3D-Scatterplot-Maria.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[15]:
-
-
-# libraries
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+#
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import matplotlib.cm as cmx
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import ListedColormap
+#
 # Dataset
 df = pd.read_csv("Tab-Morph.csv")
-
-#np.random.seed(100)
-#N = 25
-#colors = np.random.rand(N)
-
-# plot
+#
+# Plotting
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(df.plate_maria, df.sedim_thick, df.slope_angle, c='#0095d9', edgecolors='grey', s=130, alpha=.7)
+ax.scatter(df.plate_maria, df.sedim_thick, df.slope_angle,
+           c='#0095d9', edgecolors='grey', s=130, alpha=.7)
 ax.view_init(30, 185)
-plt.title('Mariana Plate \ncorrelation of the factors: \n sediment thickness, slope steepness, bathymetry', 
-          fontsize=10, fontfamily='serif')
+plt.title('Mariana Plate \ncorrelation of the factors: \n sediment thickness, slope steepness, bathymetry', fontsize=10, fontfamily='serif')
 plt.show()
-
-
-# In[ ]:
-
-
-
-
